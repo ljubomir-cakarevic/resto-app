f153a2856da724791a17ee86dca9a85285e522b9
ljubomir.cakarevic <ljubomir.cakarevic@gmail.com>
2020-12-25 21:25:42 +0100

    first commit

---
+++ src/app/update-resto/update-resto.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, EmailValidator} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RestoService} from '../resto.service';
+
+@Component({
+  selector: 'app-update-resto',
+  templateUrl: './update-resto.component.html',
+  styleUrls: ['./update-resto.component.css']
+})
+export class UpdateRestoComponent implements OnInit {
+  alert: boolean = false;
+  editResto = new FormGroup({
+    name: new FormControl(''),
+    address: new FormControl(''),
+    email: new FormControl('')
+  })
+
+  constructor(private router:ActivatedRoute, private resto:RestoService) { }
+
+  ngOnInit(): void {
+    console.warn(this.router.snapshot.params.id)
+    this.resto.getCurrentResto(this.router.snapshot.params.id)
+    .subscribe((result:any)=>{
+      this.editResto = new FormGroup({
+        name: new FormControl(result['name']),
+        address: new FormControl(result['email']),
+        email: new FormControl(result['address'])
+      })
+    })
+  }
+
+  collection(){
+    console.warn(this.editResto.value)
+    this.resto.updateResto(this.router.snapshot.params.id, this.editResto.value).subscribe((result)=>
+    this.alert=true
+    )
+  }
+  closeAlert(){
+    this.alert=false
+  }
+
+}
